a3c6d9df01dbc6a20422dc195aa90943a50c39e4
Baddog2110 <74144042+Baddog2110@users.noreply.github.com>
2020-11-20 17:40:55 +0300

    Add files via upload

---
+++ TestClass (1).java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.*;
+public class TestClass
+{
+    public static void main(String arg[])
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("type collection: ");
+        int typeCollection = in.nextInt();
+        GameItem gameItem = new GameItem(typeCollection % 3);
+        int answer = 0;
+        while (true)
+        {
+            System.out.println(answer);
+            if (gameItem.empty1())
+            {
+                System.out.println("second " + answer);
+                return;
+            }
+            else if (gameItem.empty2())
+            {
+                System.out.println("first " + answer);
+                return;
+            }
+
+            if (gameItem.getLast1() < gameItem.getLast2())
+                gameItem.replaceCard(1);
+            else
+                gameItem.replaceCard(2);
+            ++answer;
+        }
+
+    }
+}
